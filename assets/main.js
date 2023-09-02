94dcf40d940359b43e00940c3565a2c72941e994
LaryKorach <bz-support@yandex.ru>
2023-09-02 14:38:37 +0300

    02-09-2023 14:38

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,8 @@
 
 function event_description(el){
     
-    for(i=0; i<document.getElementsByClassName("item").length; i++){
-        document.getElementsByClassName("item")[i].style.display = "none";
+    for(i=0; i<document.getElementsByClassName("date-block").length; i++){
+        document.getElementsByClassName("date-block")[i].style.display = "none";
     }
     
     time = el.children[0].innerText;
@@ -23,8 +23,8 @@ function event_description(el){
 
 function all_show(){
     document.getElementById("description_block").style.display = "none";
-    for(i=0; i<document.getElementsByClassName("item").length; i++){
-        document.getElementsByClassName("item")[i].style.display = "flex";
+    for(i=0; i<document.getElementsByClassName("date-block").length; i++){
+        document.getElementsByClassName("date-block")[i].style.display = "block";
     }
 }
 
@@ -48,6 +48,48 @@ document.addEventListener('DOMContentLoaded', function(){
             event_description(el);
         }
     } 
+    
+    
+    //  преобразование дат в текст
+    
+    month_names = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    
+    var NowDate = new Date(); 
+    now_date = NowDate.getFullYear() +'-'+('0' + (NowDate.getMonth()+1)).slice(-2) + '-'+('0' + NowDate.getDate()).slice(-2);  
+    
+    var TomorrowDate = new Date(NowDate.getTime() + (24 * 60 * 60 * 1000));
+    tomorrow_date = TomorrowDate.getFullYear() +'-'+('0' + (TomorrowDate.getMonth()+1)).slice(-2) + '-'+('0' + TomorrowDate.getDate()).slice(-2);  
+    
+    var TomorrowDate2 = new Date(NowDate.getTime() + (48 * 60 * 60 * 1000));
+    tomorrow_date2 = TomorrowDate2.getFullYear() +'-'+('0' + (TomorrowDate2.getMonth()+1)).slice(-2) + '-'+('0' + TomorrowDate2.getDate()).slice(-2);  
+    
+
+    
+    for(i=0; i<document.getElementsByClassName("date-name").length; i++){
+        el = document.getElementsByClassName("date-name")[i];
+        date = el.innerText;
+        
+        month = el.innerText.substr(5, 2);
+        if(month[0] == 0) month = month[1];
+        month_str = month_names[month-1]
+        
+        day = el.innerText.substr(8, 2);
+        if(day[0] == 0) day = day[1];
+        
+        console.log(month_str)
+        
+        if(date == now_date){
+            document.getElementsByClassName("date-name")[i].innerHTML = "<red>Сегодня</red>, "+day+" "+month_str;
+            document.getElementsByClassName("date-name")[0].parentNode.children[1].classList.add("date-today");
+        }
+        
+        if(date == tomorrow_date) document.getElementsByClassName("date-name")[i].innerHTML = "Завтра, "+day+" "+month_str
+        if(date == tomorrow_date2) document.getElementsByClassName("date-name")[i].innerHTML = "Послезавтра, "+day+" "+month_str
+        
+    }
+    
+    
+    
 
 }, false);
 
